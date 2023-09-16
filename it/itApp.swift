5c860cf48f9394f25a850d105202448e982d1ca9
karirin <karirin419@gmail.com>
2023-09-16 14:21:36 +0900

    Initial Commit

---
+++ it/itApp.swift
@@ -0,0 +1,17 @@
+//
+//  itApp.swift
+//  it
+//
+//  Created by hashimo ryoya on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
